5246a2d06d8a8491c6775608ad118691a21c8b7a
Adam Mroz <MrozAdam1992@gmail.com>
2022-01-20 22:41:34 +0100

    URL handler

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -2,11 +2,34 @@ import Foundation
 
 struct WeatherManager {
     
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=6bc84d33bc7a4d2557ade60cca5070a2&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=6bc84d33bc7a4d2557ade60cca5070a2&units=metric"
     
     func fetchWeather( cityName: String ) {
-        let urlString = " \(weatherURL)&q=\(cityName)"
-        print(urlString)
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+//            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
+            task.resume()
+            
+        }
+        
     }
     
 }
