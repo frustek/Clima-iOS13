67a0379bee37788a40878adc3a181ef1728fa3fb
Adam Mroz <MrozAdam1992@gmail.com>
2022-01-22 12:38:07 +0100

    JSON data parse

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -16,18 +16,34 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
 //            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
             let task = session.dataTask(with: url) { (data, response, error) in
+                
                 if error != nil {
                     print(error!)
                     return
                 }
                 
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString!)
+//                    self.parseJSON(weatherData: safeData)
+                    parseJSON(weatherData: safeData)
+//                    let dataString = String(data: safeData, encoding: .utf8)
+//                    print(dataString!)
                 }
             }
             task.resume()
             
+        }
+        
+    }
+    
+    func parseJSON(weatherData: Data) {
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
         
     }
