3266dc2ec923b0ca2d21643c374bc6f20aaa0c44
Adam Mroz <MrozAdam1992@gmail.com>
2022-01-22 13:44:08 +0100

    MVC model

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -14,19 +14,14 @@ struct WeatherManager {
         if let url = URL(string: urlString) {
             
             let session = URLSession(configuration: .default)
-//            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
             let task = session.dataTask(with: url) { (data, response, error) in
                 
                 if error != nil {
                     print(error!)
                     return
                 }
-                
                 if let safeData = data {
-//                    self.parseJSON(weatherData: safeData)
                     parseJSON(weatherData: safeData)
-//                    let dataString = String(data: safeData, encoding: .utf8)
-//                    print(dataString!)
                 }
             }
             task.resume()
@@ -38,36 +33,19 @@ struct WeatherManager {
     func parseJSON(weatherData: Data) {
         
         let decoder = JSONDecoder()
-        
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].id)
-            print(getConditionName(weatherId: decodedData.weather[0].id))
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let weather =  WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
         
     }
-    func getConditionName(weatherId: Int) -> String {
-        
-        switch weatherId {
-            case 200...232:
-                return "cloud.bolt"
-            case 300...321:
-                return "cloud.drizzle"
-            case 500...531:
-                return "cloud.rain"
-            case 600...622:
-                return "cloud.snow"
-            case 701...781:
-                return "cloud.fog"
-            case 800:
-                return "sun.max"
-            case 801...804:
-                return "cloud.bolt"
-            default:
-                return "cloud"
-        }
-        
-    }
+    
 }
